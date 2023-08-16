3e5d7e3c6ede59c1a8226ad3d09a07c058fd1f38
Hemapriya17 <99464427+Hemapriya17@users.noreply.github.com>
2023-08-16 14:54:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+      purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   darkMode: false,
   theme: {
     fontSize: {
